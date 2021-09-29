5e7089c10556fafe4452b32cc70d0a0c0ddad419
Kia Hooper <kiahooper@hotmail.com>
2021-09-29 19:06:45 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './globalStyles/App.scss';
+import Navbar from './components/Navbar/Navbar';
+import Menu from './components/Menu/Menu';
+import Home from './components/Home/Home';
+import Projects from './components/Projects/Projects';
+import About from './components/About/About'
+import Contact from './components/Contact/Contact';
+import ScrollPagination from './components/ScrollPagination/ScrollPagination';
+import SocialSidebar from './components/SocialSidebar/SocialSidebar';
+import { useState, useEffect } from 'react';
+
+
 
 function App() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [visibleElement, setVisibleElement] = useState("home");
+
+  useEffect(() => {
+
+    const components = document.querySelectorAll(".sections > *");
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setVisibleElement(entry.target.id);
+        }
+        
+      })
+    }, {threshold: 1});
+
+    components.forEach(component => {
+      observer.observe(component);
+    })
+
+    return () => {
+      components.forEach(component => {
+        observer.unobserve(component);
+      })
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"app " + `app_${visibleElement}`}>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} visibleElement={visibleElement} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <ScrollPagination visibleElement={visibleElement}/>
+      <SocialSidebar />
+      <div className="sections">
+        {/* <Home visibleElement={visibleElement} /> */}
+        {/* <Projects visibleElement={visibleElement} /> */}
+        <About visibleElement={visibleElement}/>
+        {/* <Contact visibleElement={visibleElement}/> */}
+      </div>
     </div>
   );
 }
